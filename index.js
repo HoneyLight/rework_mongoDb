aa5d8865de5b0267661895183174b3d75a32f8e2
HoneyLight <chiketaradeborah11@gmail.com>
2023-09-12 11:49:50 +0100

    second commit

--- index.js
+++ index.js
@@ -4,14 +4,25 @@
 let express = require('express')
 let exhbs = require('express-handlebars')
 let app = express()
-let PORT = 6000
+let mongoose = require('mongoose')
+let PORT = 5000
+let CON_STR = "mongodb://localhost:27017/finance"    //we wont be using this one now cos we are using the online one as we are connected to the internet.
+let ONLINE_CON = "mongodb+srv://chiketaradeborah11:d0yCzz2bNE1DmtvI@bee-cluster.9gnn88f.mongodb.net/markets"    //the 'markets' here is the name you choose to call your database.
 
-app.engine('hbs', exhbs.create({
+// any mongodb server runs by default on port 27017
 
+mongoose.connect(ONLINE_CON, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connection.on('open', () => console.log("Mongo server connected"))
+mongoose.connection.on('error', (e) => console.log(e))
+
+app.engine('hbs', exhbs.engine({
+    extname: "hbs",
+    layoutsDir: "views",
+    defaultLayout: "main"
 }))
 
 app.set('view engine' , "hbs")
-app.set("views", "viwes")
+app.set("views", "views")
 
 app.use(express.static(__dirname+ '/public'))
 
@@ -20,4 +31,4 @@ app.get('/', function(req,res){
 })
 
 app.listen(PORT)
-console.log("app running on port :" + PORT )
+console.log("app running on port http://localhost:" + PORT )
